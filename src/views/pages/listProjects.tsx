a5422e428043e28cc26f64c048e31297719cd0e4
Pierre Duchateau <pad@dabla.com>
2021-04-13 11:51:11 +0200

    :star2:  Add project list filter for abandons

--- src/views/pages/listProjects.tsx
+++ src/views/pages/listProjects.tsx
@@ -187,7 +187,7 @@ export default function ListProjects({
                   </select>
                 </div>
                 <div style={{ marginTop: 15 }}>
-                  <div style={{ marginLeft: 2 }}>Classés/Eliminés</div>
+                  <div style={{ marginLeft: 2 }}>Classés/Eliminés/Abandons</div>
                   <select
                     name="classement"
                     className={hasNonDefaultClassement ? 'active' : ''}
@@ -197,6 +197,7 @@ export default function ListProjects({
                     <option value="">Tous</option>
                     <option value="classés">Classés</option>
                     <option value="éliminés">Eliminés</option>
+                    <option value="abandons">Abandons</option>
                   </select>
                 </div>
 
